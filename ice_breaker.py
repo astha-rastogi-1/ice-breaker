0e7863bafb1e0d1dc4ab1e7236d562edd9da81e1
Astha <astha.rastogi2000@gmail.com>
2023-10-05 17:04:18 +0530

    added a linkedin scraper to get summary of

--- ice_breaker.py
+++ ice_breaker.py
@@ -4,6 +4,8 @@ from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 
+from third_parties.linkedin import scrape_linkedin_profile
+
 load_dotenv()
 information = """
     Elon Reeve Musk (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a business magnate and investor. Musk is the founder, chairman, CEO and chief technology officer of SpaceX; angel investor, CEO, product architect and former chairman of Tesla, Inc.; owner, chairman and CTO of X Corp.; founder of the Boring Company; co-founder of Neuralink and OpenAI; and president of the Musk Foundation. He is the wealthiest person in the world, with an estimated net worth of US$232 billion as of September 2023, according to the Bloomberg Billionaires Index, and $253 billion according to Forbes, primarily from his ownership stakes in both Tesla and SpaceX.[4][5]
@@ -11,10 +13,9 @@ information = """
 
 if __name__=="__main__":
     print("Hello langchain")
-    # print(os.environ['OPENAI_API_KEY'])
-
+    
     summary_template = """
-        given the information {information} about a person I want you to create:
+        given the LinkedIN information {information} about a person I want you to create:
         1. a short summary
         2. two interesting facts about them
     """
@@ -25,4 +26,6 @@ if __name__=="__main__":
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
-    print(chain.run(information=information))
+    linkedin_data = scrape_linkedin_profile("https://gist.githubusercontent.com/emarco177/0d6a3f93dd06634d95e46a2782ed7490/raw/fad4d7a87e3e934ad52ba2a968bad9eb45128665/eden-marco.json")
+
+    print(chain.run(information=linkedin_data.json()))
