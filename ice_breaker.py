ddf19bee110436a6b3fcefa6d5f1e1ca75a312f3
Astha <astha.rastogi2000@gmail.com>
2023-10-06 11:13:38 +0530

    added linkedin url searching and fetching using agents

--- ice_breaker.py
+++ ice_breaker.py
@@ -4,7 +4,9 @@ from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 
+
 from third_parties.linkedin import scrape_linkedin_profile
+from third_parties.linkedin_lookup_agent import lookup
 
 load_dotenv()
 information = """
@@ -13,7 +15,10 @@ information = """
 
 if __name__=="__main__":
     print("Hello langchain")
-    
+
+    linkedin_profile_url = lookup(name="Eden Marco")
+    print(f"URL: {linkedin_profile_url}")
+
     summary_template = """
         given the LinkedIN information {information} about a person I want you to create:
         1. a short summary
