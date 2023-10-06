fdd84cda1c100802bc7bee0f6c28b2ee299ed9df
Astha <astha.rastogi2000@gmail.com>
2023-10-06 11:35:20 +0530

    added output parser using pydantic, reformatted code

--- ice_breaker.py
+++ ice_breaker.py
@@ -4,33 +4,48 @@ from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 
-
+from output_parsers import PersonIntel, person_intel_parser
 from third_parties.linkedin import scrape_linkedin_profile
 from third_parties.linkedin_lookup_agent import lookup
 
 load_dotenv()
-information = """
-    Elon Reeve Musk (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a business magnate and investor. Musk is the founder, chairman, CEO and chief technology officer of SpaceX; angel investor, CEO, product architect and former chairman of Tesla, Inc.; owner, chairman and CTO of X Corp.; founder of the Boring Company; co-founder of Neuralink and OpenAI; and president of the Musk Foundation. He is the wealthiest person in the world, with an estimated net worth of US$232 billion as of September 2023, according to the Bloomberg Billionaires Index, and $253 billion according to Forbes, primarily from his ownership stakes in both Tesla and SpaceX.[4][5]
-"""
+# information = """
+#     Elon Reeve Musk (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a business magnate and investor. Musk is the founder, chairman, CEO and chief technology officer of SpaceX; angel investor, CEO, product architect and former chairman of Tesla, Inc.; owner, chairman and CTO of X Corp.; founder of the Boring Company; co-founder of Neuralink and OpenAI; and president of the Musk Foundation. He is the wealthiest person in the world, with an estimated net worth of US$232 billion as of September 2023, according to the Bloomberg Billionaires Index, and $253 billion according to Forbes, primarily from his ownership stakes in both Tesla and SpaceX.[4][5]
+# """
 
-if __name__=="__main__":
-    print("Hello langchain")
 
+def ice_break(name: str) -> PersonIntel:
     linkedin_profile_url = lookup(name="Eden Marco")
-    print(f"URL: {linkedin_profile_url}")
+    # print(f"URL: {linkedin_profile_url}")
 
     summary_template = """
         given the LinkedIN information {information} about a person I want you to create:
         1. a short summary
         2. two interesting facts about them
+        \n{format_instructions}
     """
 
-    summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"],
+        template=summary_template,
+        partial_variables={
+            "format_instructions": person_intel_parser.get_format_instructions()
+        },
+    )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
-    linkedin_data = scrape_linkedin_profile("https://gist.githubusercontent.com/emarco177/0d6a3f93dd06634d95e46a2782ed7490/raw/fad4d7a87e3e934ad52ba2a968bad9eb45128665/eden-marco.json")
+    linkedin_data = scrape_linkedin_profile(
+        "https://gist.githubusercontent.com/emarco177/0d6a3f93dd06634d95e46a2782ed7490/raw/fad4d7a87e3e934ad52ba2a968bad9eb45128665/eden-marco.json"
+    )
 
-    print(chain.run(information=linkedin_data.json()))
+    result = chain.run(information=linkedin_data.json())
+    print(result)
+    return person_intel_parser.parse(result)
+
+
+if __name__ == "__main__":
+    print("Hello LangChain!")
+    ice_break("Eden Marco")
--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -1,16 +1,16 @@
 import os
 import requests
 
+
 def scrape_linkedin_profile(linkedin_profile_url: str):
     """Scrape info from LinkeIn profiles,
     Manually scrape the info from the LinkedIn profile"""
-    
+
     # api_endpoint = "https://gist.githubusercontent.com/emarco177/0d6a3f93dd06634d95e46a2782ed7490/raw/fad4d7a87e3e934ad52ba2a968bad9eb45128665/eden-marco.json"
     # header_disc = {"Authorization": f"Bearer {os.environ.get("PROXYCURL_API_KEY")}"}
-    
+
     response = requests.get(
-        params={"url": linkedin_profile_url},
-        url=linkedin_profile_url
+        params={"url": linkedin_profile_url}, url=linkedin_profile_url
     )
 
     return response
